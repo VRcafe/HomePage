02d254ee9d035fd10019dfdda33acd3e87fa50f2
mori toshiki <toshiki@morinoMacBook-Air.local>
2023-10-28 14:09:27 +0900

    add: Drawer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from 'react'
-import { AppBar, Toolbar, Slide, useScrollTrigger, Typography } from '@mui/material'
+import React, { FC, useState } from 'react'
+import { AppBar, Toolbar, Slide, useScrollTrigger, Typography, Drawer, Button, List, ListItem } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
 
 interface Props {
   window?: () => Window
@@ -20,6 +21,7 @@ const HideOnScroll: FC<Props> = (props) => {
 }
 
 export const Header: FC = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
   return (
     <>
       <HideOnScroll>
@@ -27,6 +29,43 @@ export const Header: FC = () => {
           <Toolbar>
             <img src="/images/logo.png" height={100} width={100} alt="" />
             <Typography color="black" component="label" ml={1} sx={{ flexGrow: 1 }} variant="h6"></Typography>
+            <Button onClick={() => setIsDrawerOpen(true)}>
+              <MenuIcon fontSize="large" sx={{ color: 'black' }} />
+            </Button>
+            <Drawer anchor="right" open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
+              <List sx={{ width: '30vw' }}>
+                <ListItem style={{ display: 'inline-block' }}>
+                  <Typography align="center" marginBlock={2}>
+                    OUR MISSIONS
+                  </Typography>
+                </ListItem>
+                <ListItem style={{ display: 'inline-block' }}>
+                  <Typography align="center" marginBlock={2}>
+                    CONTENTS
+                  </Typography>
+                </ListItem>
+                <ListItem style={{ display: 'inline-block' }}>
+                  <Typography align="center" marginBlock={2}>
+                    CASES
+                  </Typography>
+                </ListItem>
+                <ListItem style={{ display: 'inline-block' }}>
+                  <Typography align="center" marginBlock={2}>
+                    ABOUT US
+                  </Typography>
+                </ListItem>
+                <ListItem style={{ display: 'inline-block' }}>
+                  <Typography align="center" marginBlock={2}>
+                    BLOG
+                  </Typography>
+                </ListItem>
+                <ListItem style={{ display: 'inline-block' }}>
+                  <Typography align="center" marginBlock={2}>
+                    ONLINE SHOP
+                  </Typography>
+                </ListItem>
+              </List>
+            </Drawer>
           </Toolbar>
         </AppBar>
       </HideOnScroll>
