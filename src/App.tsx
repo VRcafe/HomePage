56ad49ef8a9c7370a8c9622ecc3c2832acd4c70d
mori toshiki <toshiki@morinoMacBook-Air.local>
2023-11-01 17:19:51 +0900

    add: hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import React from 'react'
 import { FC } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import IndexPage from './pages/index'
 import MenuPage from './pages/menu/detail'
 import RepresentativePage from './pages/introduction/representative'
@@ -14,7 +14,7 @@ const Pages = () => {
     <>
       <ScrollToTop />
       <Routes>
-        <Route path="/HomePage/" element={<IndexPage />} />
+        <Route path="/HomePage" element={<IndexPage />} />
         <Route path="/HomePage/menu" element={<MenuPage />} />
         <Route path="/HomePage/cases" element />
         <Route path="/HomePage/representative" element={<RepresentativePage />} />
@@ -28,9 +28,9 @@ const Pages = () => {
 const App: FC = () => {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Pages />
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
